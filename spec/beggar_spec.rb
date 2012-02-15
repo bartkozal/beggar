f4e9310812cd0283a3e5833855ecd1b2b5016413
Bartlomiej Kozal <bkzl@me.com>
2012-02-15 23:22:34 +0100

    Configuration file

---
+++ spec/beggar_spec.rb
@@ -0,0 +1,38 @@
+require 'beggar'
+
+describe Beggar::CLI do
+  it "should load configuration on run" do
+    Beggar::CLI.should_receive(:load_configuration)
+    Beggar::CLI.run
+  end
+
+  context "load configuration" do
+    before do
+      @home = Dir.home
+    end
+
+    it "returns hash with options from yml file" do
+      configuration = YAML.load_file('./fixtures/beggar')
+      YAML.should_receive(:load_file).with("#@home/.beggar").and_return(configuration)
+      Beggar::CLI.run
+    end
+
+    it "creates .beggarc when config doesn't exists" do
+      default_configuration = {
+        "company" => "your_company_name",
+        "token" => "your_basecamp_token",
+        "projects" => {
+          "project_id" => {
+            "rate" => "your_rate"
+          }
+        }
+      }
+      output = double()
+      YAML.should_receive(:load_file).with("#@home/.beggar") { raise Errno::ENOENT }
+      File.should_receive(:open).with("#@home/.beggar", "w").and_yield(output)
+      YAML.should_receive(:dump).with(default_configuration, output)
+      $stdout.should_receive(:puts).with("Saved a new configuration file in ~/.beggar. Please fill it with proper data.")
+      Beggar::CLI.run
+    end
+  end
+end
