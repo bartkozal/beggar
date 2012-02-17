749608e52b953018329f8e488ea201511ad6ed41
Bartlomiej Kozal <bkzl@me.com>
2012-02-17 21:45:26 +0100

    Implement progress

--- lib/beggar/base.rb
+++ lib/beggar/base.rb
@@ -3,6 +3,30 @@ module Beggar
     def initialize(basecamp)
       @basecamp = basecamp
     end
+
+    def progress
+      (current_month_wdays_until(Date.today) / current_month_wdays.to_f * 100).round.to_s + "%"
+    end
+
+private
+
+    def current_month_wdays
+      current_month_wdays_until(current_month_last_day)
+    end
+
+    def current_month_wdays_until(date)
+      (current_month_first_day..date).reject { |d| [0, 6].include? d.wday }.length
+    end
+
+    def current_month_first_day
+      year = Date.today.year
+      month = Date.today.month
+      Date.new(year, month, 1)
+    end
+
+    def current_month_last_day
+      current_month_first_day.next_month - 1
+    end
   end
 end
 
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,9 +1,33 @@
 require 'beggar'
 
 describe Beggar::Base do
+  before do
+    basecamp = double()
+    @base = Beggar::Base.new(basecamp)
+  end
+
   it 'returns summary' do
     pending
     # 78% || 140.0h ± 12.0h || $5320.0 ± $456.0
+  end
+
+  describe '#progress' do
+    describe 'returning progress of working days in percents' do
+      it 'returns 62% on 17th February 2012' do
+        Date.stub(today: Date.new(2012, 2, 17))
+        @base.progress.should == "62%"
+      end
+
+      it 'returns 2nd December 2011' do
+        Date.stub(today: Date.new(2011, 12, 2))
+        @base.progress.should == "9%"
+      end
+
+      it 'returns 30th November 1988' do
+        Date.stub(today: Date.new(1988, 11, 30))
+        @base.progress.should == "100%"
+      end
+    end
   end
 end
 
